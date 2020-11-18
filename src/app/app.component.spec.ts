305a32ed59aca4ef933099b5e429e7c54d64de8b
trashe_racer <trashe.racer@protonmail.com>
2020-11-18 18:37:36 +0100

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,18 +16,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'heroku'`, () => {
+  it(`should have as title 'iwitness-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('heroku');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'heroku app is running!'
-    );
+    expect(app.title).toEqual('iwitness-project');
   });
 });
