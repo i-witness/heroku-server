d67f703f5376fe0d7854afa2a85634d5a7c89196
trashe_racer <trashe.racer@protonmail.com>
2020-11-19 03:29:31 +0100

    create modules and components for each main view
    also create a shared module

---
+++ src/app/support/support.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SupportComponent } from './support.component';
+
+@NgModule({
+  declarations: [SupportComponent],
+  imports: [CommonModule],
+})
+export class SupportModule {}
---
+++ src/app/terms/terms.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TermsComponent } from './terms.component';
+
+@NgModule({
+  declarations: [TermsComponent],
+  imports: [CommonModule],
+})
+export class TermsModule {}
