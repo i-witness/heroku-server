21f88d5544737b98b578ee205d9c18242150272e
trashe_racer <trashe.racer@protonmail.com>
2020-11-19 04:17:56 +0100

    add routing for each module component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DataComponent } from './data/data.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { SupportComponent } from './support/support.component';
+import { TermsComponent } from './terms/terms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'data', component: DataComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'support', component: SupportComponent },
+  { path: 'terms', component: TermsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
