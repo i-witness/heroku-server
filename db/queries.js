3020025004bc6195fd10298d92a9df09ac5b34ae
trashe_racer <trashe.racer@protonmail.com>
2020-11-26 20:12:34 +0100

    fix caps in query

--- db/queries.js
+++ db/queries.js
@@ -1,9 +1,9 @@
 const USER_DETAILS = `
     SELECT 
-        user_id AS userID,
-        email_addr AS email,
-        created_at AS createdAt,
-        home_country AS country
+        user_id AS "userID",
+        email_addr AS "email",
+        created_at AS "createdAt",
+        home_country AS "country"
     FROM 
         user_details`;
 
